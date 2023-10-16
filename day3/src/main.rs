88bda560726d6b07b312ab508158faa9c4b5944b
Daniel Pham <28274722+danielcpham@users.noreply.github.com>
2023-10-16 11:39:33 -0700

    finish day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,36 +1,55 @@
-pub use day3::get_priority;
-use std::{collections::HashSet, str::Lines};
+use itertools::Itertools;
 
-fn part1(lines: Lines) -> i16 {
-	lines
-		.map(|line| {
-			let (first, second) = line.split_at(line.len() / 2);
-			second
-				.bytes()
-				.filter(|b| b.is_ascii() && first.contains(*b as char))
-				.map(get_priority)
-				.collect::<HashSet<i16>>()
-				.iter()
-				.sum::<i16>()
-		})
-		.sum::<i16>()
+fn get_priority(b: u8) -> u32 {
+    (match b {
+        b'a'..=b'z' => (b - b'a') + 1,
+        _ => (b - b'A') + 1 + 26,
+    }) as u32
+}
+fn get_sack_priority(line: &str) -> Option<u32> {
+    let (first, second) = line.as_bytes().split_at(line.len() / 2);
+    first
+        .iter()
+        .filter(|b| b.is_ascii_alphabetic())
+        .find(|b| second.contains(b))
+        .and_then(|b| Some(get_priority(*b) as u32))
 }
 
-fn part2(lines: Lines) -> i16 {
-	0
+fn part1(sacks: &str) -> u32 {
+    sacks.lines().filter_map(get_sack_priority).sum::<u32>()
+}
+
+fn get_badge_priorities_sum(sack_list: &str) -> u32 {
+    sack_list
+        .lines()
+        .tuples() //
+        .filter_map(|(sack1, sack2, sack3)| {
+            sack1
+                .find(|x| sack2.contains(x) && sack3.contains(x))
+                .and_then(|i| sack1.as_bytes().get(i))
+        }).map(|b| get_priority(*b))
+        .sum::<u32>()
+}
+
+
+fn part2(groups: &str) -> u32 {
+    get_badge_priorities_sum(groups)
 }
 
 fn main() {
-	let cases = include_str!("../cases.txt").lines();
+    let part1_res = part1(include_str!("../cases.txt"));
+    let res = part2(include_str!("../cases.txt"));
+    println!("{:?}", part1_res);
+    println!("{:?}", res);
 }
 
 #[cfg(test)]
 mod tests {
 
-	#[test]
-	fn test_case1() {
-		use crate::part1;
-		let input = "
+    #[test]
+    fn test_case1() {
+        use crate::part1;
+        let input = "
 vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
@@ -38,8 +57,13 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
 ";
-		let res = part1(input.lines());
-		assert_eq!(res, 157);
-	}
-
+        let res = part1(input);
+        assert_eq!(res, 157);
+    }
+	#[test]
+    fn test_get_badge() {
+        use super::get_badge_priorities_sum;
+        let input = include_str!("../day3b_test.txt");
+        get_badge_priorities_sum(input);
+    }
 }
